eea3d9b7fe039d06f1a310a2e0c4086f43c69772
Neamul01 <neamulhaque2002@gmail.com>
2022-04-21 22:09:56 +0600

    in mongo server user added system done

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+//use midleware
+app.use(cors());
+app.use(express.json());
+
+//user:dbuser1
+//pass:BvH7CYMiFtaY3jxG
+
+
+
+const uri = "mongodb+srv://dbuser1:BvH7CYMiFtaY3jxG@cluster0.bo3cb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+
+        const userCollection = client.db('foodExpress').collection('user');
+        const user = { name: "Noman", email: "Noman@gmail.com" };
+
+        const result = await userCollection.insertOne(user);
+        console.log(`a user was inserted a with id:${result.insertedId} `);
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('runnign server')
+})
+
+
+app.listen(port, () => {
+    console.log('CRUD server running in port', port)
+})
